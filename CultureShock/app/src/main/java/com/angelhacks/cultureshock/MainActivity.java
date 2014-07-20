d0654d1bcb89730f8b1966c7091af743c183d19f
Rohit Tigga <rrtigga@ucdavis.edu>
2014-07-19 18:50:39 -0700

    fds
    fds

--- CultureShock/app/src/main/java/com/angelhacks/cultureshock/MainActivity.java
+++ CultureShock/app/src/main/java/com/angelhacks/cultureshock/MainActivity.java
@@ -1,9 +1,12 @@
 package com.angelhacks.cultureshock;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,6 +14,10 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         setContentView(R.layout.activity_main);
     }
 
